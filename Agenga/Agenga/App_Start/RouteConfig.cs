d1e01dee165a4edde60e1ee90d3998d5029b7ce5
Aziox <azioxgaming@hotmail.com>
2019-01-31 02:41:48 +0100

    j'ai tester

---
+++ Agenga/Agenga/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Agenga
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            //je ne suis pas sur de moi du tout
+
+            routes.MapRoute(
+                name: "Broker",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Broker", action = "addBroker", id = UrlParameter.Optional }
+                );
+        }
+    }
+}
---
+++ Agenga/Agenga/Models/Broker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenga.Models
+{
+    public class Broker
+    {
+        public int idBroker { get; set; }
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+        public string mail { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
